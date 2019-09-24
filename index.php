35487e8e4ca37309201aa680861d46bdbb3cfbe0
codebuilderspk <53963529+codebuilderspk@users.noreply.github.com>
2019-09-24 13:16:56 +0500

    making better

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
 echo "krisroup";
+?>
+<html>
+  <head>
+    <style>
+      h1{
+        color:lightblue;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>This is really fantastic ! Heroku</h1>
+  </body>
+</html>
